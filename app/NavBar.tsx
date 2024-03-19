f7535f1b41eba29069e7ecf74f26c613ad10b087
Alexander Rubino <alexrubino05@gmail.com>
2024-03-19 13:34:01 +0100

    Init

---
+++ app/NavBar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 mb-5 px-5 border-b h-14 items-center">
+      <Link href="/">Logo</Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            href={link.href}
+            key={link.href}
+            className={`${
+              link.href === currentPath ? "text-black" : "text-zinc-500"
+            } hover:text-zinc-800 transition-colors`}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
